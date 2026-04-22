7a190ad552cf03c244dfd8d7b778fa76721a4115
Aoilian <i90508332@gmail.com>
2026-04-22 20:14:33 +0200

    Modifs fonction designer gagnant

--- score.c
+++ score.c
@@ -9,7 +9,7 @@ typedef struct{
 	char prenom[255];
 }Perso;
 
-//Verifier que le joueur est valide
+//On verifie que le joueur est valide
 int PersoValide(Perso a){
 		for(int i = 0; i < 7; i++ ){
 				if(a.carte[i] < 0 || a.carte[i] > 12){
@@ -33,14 +33,18 @@ unsigned int calculScoreMancheTot(Perso a, int bonus){
 	
 }
 
-void reinitialiserJoueur(Perso* joueurs, int taille){
-		if(joueurs == NULL || taille < 2){
+
+void reinitialiserJoueur(Perso* joueurs, int nbjoueur){
+		if(joueurs == NULL || nbjoueur < 2){
                 	printf("Erreur\n");
                 	exit(1);
-                }
-		for(int i = 0; i < taille; i++){
+		}
+		for(int i = 0; i < nbjoueur; i++){
+			//On remet à 0 le nombre de carte que les joueurs possèdent 
+			(joueurs+i)->nbcarte = 0;
 			for(int j = 0; j < 7; j++){
-				joueurs->carte[j] = 0;
+				//On supprime la main des joueurs quand la manche est terminé
+				(joueurs + i)->carte[j] = 0;
 			}
 		}
 }
@@ -59,15 +63,19 @@ bool FinDePartie(Perso* joueurs, unsigned int* pioche, int taille){
 	return false;
 }
 
-void designerGagnant(Perso* joueurs, int taille){
+Perso* designerGagnant(Perso* joueurs, int taille){
 	    if(joueurs == NULL || taille < 2){
                 printf("Erreur\n");
                 exit(1);
         }
-		int bonus = 0;
-	    //On initialise le max avec la valeur du premier joueur 
+
+		int bonus = 0; //A modifier
+
+	    //On initialise le max et son adresse avec celle du premier joueur
 	    unsigned int max = calculScoreMancheTot(joueurs[0],bonus);
 	    Perso* adresseMax; 
+		adresseMax = joueurs;
+
 	    //On fait une recherche de maximum sur le score pour renvoyer l'adresse et le score du gagnant
 	    for(int j = 1; j < taille; j++){
 			if(calculScoreMancheTot(joueurs[j],bonus) >  max){
@@ -77,27 +85,35 @@ void designerGagnant(Perso* joueurs, int taille){
 			//Faire le cas ou 2 Joueurs ont le même score final
 	     }
 	     printf("Le gagnant est : %s  avec un score de  %u   \n",adresseMax->prenom, max);
+		 //On retourne l'adresse du joueur qui a gagné
+		 return adresseMax;
 }
 
 //On enregistre le Nom de chaque gagnant de la partie
-void savejoueur(Perso* a, int nbjoueur){
-	if(a == NULL){
+void Enregistrejoueurs(Perso* a, int nbjoueur){
+	if(a == NULL || nbjoueur < 2){
             printf("Erreur\n");
             exit(5);
     }
+
+	//On ouvre le fichier Fliptech.txt
 	FILE* fichier = NULL;
 	fichier = fopen("Fliptech.txt", "w+");
+
 	for(int i = 0; i < nbjoueur; i++){
 		char choix;
 		unsigned int score = calculScoreMancheTot(a[9], -1); // A MODIFIER
 		while(scanf("%c",&choix) != 1 && choix != 'O' && choix != 'N'){ //MARCHE PAS
 					printf("Saisi invalide, veuillre réessayer. \n");
-					while(getchar() != '\n'); //On vide le buffer après la saisi de l'utilisateur
+					while(getchar() != '\n'); //On vide le tampon après la saisi de l'utilisateur
 		}
 		if(choix == 'O'){
-			fprintf(fichier, "Prenom : %s | score : %u \n", a->prenom, score); //A MODIFIER
+			//
+			if((a + i) == designerGagnant(a, nbjoueur)){
+						fprintf(fichier, "Prenom : %s | score : %u | vainqueur \n", a->prenom, score);
+			}
+			fprintf(fichier, "Prenom : %s | score : %u \n", a->prenom, score);
 		}
-		//Faire le cas ou le joueur dis oui et il a gagné 
 	}
 }
 
