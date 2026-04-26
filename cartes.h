cb986f63facb53b5b75f914ba61bd1b705901ab8
Aoilian <i90508332@gmail.com>
2026-04-26 13:01:36 +0200

    Suppression des structures

--- cartes.h
+++ cartes.h
@@ -13,30 +13,6 @@
 // #define SPECIAL2 2  /* TROIS A LA SUITE */
 // #define SPECIAL3 3  /* SECONDE CHANCE */
 
-/* une carte du jeu */
-
-typedef struct {
-    char type; /* 'N' = carte numéro, 'B' = carte bonus, 'S' = carte spéciale */
-    int numero;  /* de 12 à 0 */
-    int bonus; /* 6 cartes bonus */
-//    int speciale; /* 9 cartes spéciales */
-} Carte;
-
-/* le paquet de cartes */
-
-typedef struct {
-    Carte cartes[FLIP7];
-    int nbCartes; /* nombre de cartes restantes dans la pile */
-} Paquet;
-
-/* les statistiques */
-
-typedef struct {
-    int nbNumero; /* nombre de cartes numéro piochées */
-    int nbBonus; /* nombre de cartes bonus piochées */
-//    int nbSpeciale; /* nombre de cartes spéciale piochées */
-} Stats;
-
 void afficherPaquet(Paquet *p);
 void creerPaquet(Paquet *p);
 void melanger(Paquet *p);
